c78b3b6443311603fa9d7d73ffb47522317e8529
William <willtheorangeguy@users.noreply.github.com>
2023-11-07 09:32:25 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "williamvdg.me", // replace this with your deployed domain
+  site: "https://williamvdg.me", // replace this with your deployed domain
   integrations: [
     tailwind({
       applyBaseStyles: false,
